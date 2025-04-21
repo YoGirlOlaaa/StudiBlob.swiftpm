edd64b9dec062057eee19622270968e2b648cd29
Ola Oleksiak <aoleksiak8215@stu.d214.org>
2025-04-21 10:49:28 -0500

    added images

--- Store.swift
+++ Store.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct Store: View {
+    let images = ["image1", "image2", "image3", "image4"]
+    
     var body: some View {
         VStack {
             
             Text("Store").font(.system(size: 35, weight: .semibold, design: .serif))
                 .frame(alignment: .top)
                 .padding()
+            
+                ImageSliderView()
+                    .padding()
+            
+            
         }
         
         .edgesIgnoringSafeArea(.all) // This ensures the page takes up the full screen
