904eb70a608e8996637e318f49361fed9be0fc3b
Ola Oleksiak <aoleksiak8215@stu.d214.org>
2025-04-21 10:12:15 -0500

    added text to the store

--- Store.swift
+++ Store.swift
@@ -10,17 +10,10 @@ import SwiftUI
 struct Store: View {
     var body: some View {
         VStack {
-            Text("This is the full screen page")
-                .font(.largeTitle)
-                .foregroundColor(.white)
             
-            Button("Back to Main Page") {
-                // Action to navigate back to the main page
-            }
-            .padding()
-            .background(Color.red)
-            .foregroundColor(.white)
-            .cornerRadius(10)
+            Text("Store").font(.system(size: 35, weight: .semibold, design: .serif))
+                .frame(alignment: .top)
+                .padding()
         }
         
         .edgesIgnoringSafeArea(.all) // This ensures the page takes up the full screen
