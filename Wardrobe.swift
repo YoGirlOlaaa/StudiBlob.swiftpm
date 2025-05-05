6693423c347855eb637f119171c71eaf2fb51052
Cassandra Botnari <cbotnari8050@stu.d214.org>
2025-05-05 11:04:23 -0500

    made the scrollview baseline for the wardrobe (will edit to make it connect to our app)

--- Wardrobe.swift
+++ Wardrobe.swift
@@ -8,7 +8,44 @@
 import SwiftUI
 
 struct Wardrobe: View {
+    let menuItems = ["Start Game", "Options", "High Scores", "Exit", "Settings", "Credits", "Help"]
+//    these have to be replaced with the bought items
     var body: some View {
-        
-    }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        
+                        ForEach(menuItems, id: \.self) { item in
+                            Button(action: {
+                                
+                            }) {
+                                Text(item)
+                                    .font(.title2)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .frame(width: 200)
+                                    .shadow(radius: 5)
+//                                make this fit and look how you want it to
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .frame(height: 150)
+            }
+           
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+#Preview {
+    Wardrobe()
 }
