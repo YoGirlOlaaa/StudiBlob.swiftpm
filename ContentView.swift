f48c786359e83b1a62beb2ad575a7ac9ebdde3eb
Cassandra Botnari <cbotnari8050@stu.d214.org>
2025-05-05 10:32:59 -0500

    started the layout for the bought items (putting it on blober)

--- ContentView.swift
+++ ContentView.swift
@@ -6,6 +6,7 @@ struct ContentView: View {
     @State var items: [AssignmentItem] = []
     @State var selectedPoints: Double = 50.0
     @AppStorage("totalPoints") var totalPoints: Int = 0
+    @State var asset = ""
     var body: some View {
         //StudiBlob backround
         
@@ -39,12 +40,14 @@ struct ContentView: View {
                                 .onDelete{ indexSet in items.remove(atOffsets: indexSet)
                                     totalPoints += Int(selectedPoints)
                                 }
-                                    
-                                Image("Blober")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 9000, height: 800)
+                                HStack{
+                                    Image("Blober")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 9000, height: 800)
                                     Spacer()
+                                    Image(asset)
+                                }
                             }
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
                         }
