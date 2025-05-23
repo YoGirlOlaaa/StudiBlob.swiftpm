49d374b903368b956447d7e76c48c915cde05fed
Cassandra Botnari <cbotnari8050@stu.d214.org>
2025-05-23 10:47:55 -0500

    added assets (shorts)

--- Wardrobe.swift
+++ Wardrobe.swift
@@ -14,17 +14,18 @@ struct Wardrobe: View {
     @Binding var assetShort: String
     let menuItems = ["Crown", "Horns", "Black Top Hat", "Hearts", "Leafy Hat", "Credits", "Help"]
     let menuEyes = ["Eyelids 1", "Eyelashes 1", "Black Top Hat", "Hearts", "Leafy Hat", "Witch Hat", "Help"]
+    let menuShort = ["Yellow Shorts", "Jean Short"]
     //    these have to be replaced with the bought items
     var body: some View {
         
         ZStack(alignment: .top){
-           // Image("Image")
-//                .resizable()
-//                .scaledToFill()
-//                .frame(maxHeight: 200)
-//                .clipped()
-//                .ignoresSafeArea(edges:.top)
-//            
+            // Image("Image")
+            //                .resizable()
+            //                .scaledToFill()
+            //                .frame(maxHeight: 200)
+            //                .clipped()
+            //                .ignoresSafeArea(edges:.top)
+            //
             
             Text("Welcome To Your Wardrobe")
                 .font(.system(size: 55, weight: .semibold, design: .serif))
@@ -95,13 +96,42 @@ struct Wardrobe: View {
                     .padding(.horizontal)
                 }
                 .frame(height: 150)
-                Spacer()
-                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        
+                        ForEach(menuShort, id: \.self) { item in
+                            Button(action: {
+                                assetShort = item
+                            })  {
+                                
+                                ZStack{
+                                    Image(item)
+                                        .resizable()
+                                    
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .frame(width: 20, height: 20)
+                                    Text(item)
+                                        .font(.title2)
+                                        .padding()
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                    
+                                        .shadow(radius: 5)
+                                }
+                                
+                                
+                                //                                make this fit and look how you want it to
+                            }
+                        }.frame(height: 300)
+                    }
+                    Spacer()
+                    
+                }
+                .padding()
             }
-            .padding()
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationBarTitleDisplayMode(.inline)
-    }
         
         
         
@@ -110,7 +140,8 @@ struct Wardrobe: View {
         
     }
     
-//    #Preview {
-//        Wardrobe(assetHat: $assetHat, assetEyes: $assetEyes)
-//    }
-
+    //    #Preview {
+    //        Wardrobe(assetHat: $assetHat, assetEyes: $assetEyes)
+    //    }
+    
+}
